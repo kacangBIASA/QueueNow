4230019d4a5e71f7645a11c2af9fe6aa34884d6d
kacangBIASA <kacangBIASA>
2025-12-26 23:07:23 +0700

    commit fixing error

--- config/midtrans.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'pro_price' => (int) env('MIDTRANS_PRO_PRICE', 25000),
-    'pro_name' => env('MIDTRANS_PRO_NAME', 'QueueNow Pro'),
-];
